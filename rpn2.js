b595bdbf6e6ee4dc78592cc363e508552d79dd7e
Manuel_Canul109/ <manuel.canul109@gmail.com>
2019-05-13 23:00:46 -0500

    TERMINACION

--- rpn2.js
+++ rpn2.js
@@ -1,258 +1,262 @@
+// el postfijo no se forma bien con condicionales (solo con operaciones aritmeticas) no me calcula bien el resultado
+
 function main(){
 
-        var rs = require('readline');
+  var rs = require('readline');
 
-        const resp = rs.createInterface({
-            input: process.stdin,
-            output: process.stdout
-          });
+  const resp = rs.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-          resp.question('Introduzca lo que vamos a calcular: ', (VALO) => {
-            
-            console.log("Validando parentesís...");
-            let expr = "2 * 3 *(3/3)";
-            let {error, index} = verify(VALO);
-            if (error === false) {
-              console.log('¡Parentesis correctos!');
-              console.log();
-              console.log("Evaluando Operandos...");
-              let sig = evaluate(VALO);
-              if(sig === true){
-                console.log("¡Operandos correctos!")
-                console.log();
-                console.log("Evaluando signos no permitidos...")
-                let {consigers,  erers} = validarErrSig(VALO);
-                
-                    if(consigers === true){
-                        console.log("¡Se detectó un error en la expresión!");
-                        console.log("¡Se hallaron caracteres invalidos!");
-                        console.log(erers);
-                    }else{
-                      console.log("¡No se detecto signo no permitido!")
-                      console.log();
-                      console.log("Separando ecuacion...");
-                      console.log(tokenize(VALO)); 
-                      console.log("¡Ecuacion separada!")
-                      console.log();
-                      console.log("Pasando a Postfijo...");
-                      console.log(infixToPostfix(tokenize(VALO))); 
-                      console.log();
-                      console.log("Evaluando y resolviendo RPN...");
-                      console.log(RPN(infixToPostfix(tokenize(VALO)))); 
-                      console.log();
-                      console.log("FINALIZADO");
-                  }
-                }
-                else{
-                  console.log("¡Se detectó un error en la expresión!");
-                }
-            } else {
-              console.log();
-              console.log('¡Se detectó un error en los parentesis, verifique su expresion!');
-            }
-            resp.close();
-        });
+    resp.question('Introduzca lo que vamos a calcular: ', (VALO) => {
+      
+      console.log("Validando parentesís...");
+      let expr = "2 * 3 *(3/3)";
+      let {error, index} = verify(VALO);
+      if (error === false) {
+        
+        console.log('¡Parentesis correctos!');
+        console.log();
+        console.log("Evaluando Operandos...");
+
+          console.log("¡Operandos correctos!")
+          console.log();
+          console.log("Separando ecuacion...");
+          console.log(tokenize(VALO)); 
+          console.log("¡Ecuacion separada!")
+          console.log("Evaluando signos no permitidos...")
+          let {consigers,  erers} = validarErrSig(VALO);
+          
+          if(consigers === true){
+              //console.log("¡Se detectó un error en la expresión!");
+              console.log("¡Se hallaron caracteres invalidos!");
+              console.log(erers);
+          }else{
+            console.log("¡No se detecto signo no permitido!")
+            console.log();
+            console.log("Pasando a Postfijo...");
+            console.log(infixToPostfix(tokenize(VALO))); 
+            console.log();
+            console.log("Evaluando y resolviendo RPN...");
+            console.log(RPN(infixToPostfix(tokenize(VALO)))); 
+            console.log();
+            console.log("FINALIZADO");
+        }            
+      } else {
+        console.log();
+        console.log('¡Se detectó un error en los parentesis, verifique su expresion!');
+      }
+      resp.close();
+    });
 
 }
 
 function verify(expr) {
-  let stack = [];
-  const allSymbols = ['{', '[', '(', ')', ']', '}'];
-  const openSymbols = ['{', '[', '('];
-  const checkSymbols = { '{': '}', '[': ']', '(': ')' };
+let stack = [];
+const allSymbols = ['{', '[', '(', ')', ']', '}'];
+const openSymbols = ['{', '[', '('];
+const checkSymbols = { '{': '}', '[': ']', '(': ')' };
 
-  for (let i = 0; i < expr.length; ++i) {
-      let token = expr[i];
-      if (allSymbols.findIndex(e => e === token) >= 0) {
-          if (openSymbols.findIndex(e => e === token) >= 0) {
-              stack.push(token);
-          } else {
-              if (stack.length === 0) {
-                  return { error: true, index: i };
-              }
+for (let i = 0; i < expr.length; ++i) {
+let token = expr[i];
+if (allSymbols.findIndex(e => e === token) >= 0) {
+    if (openSymbols.findIndex(e => e === token) >= 0) {
+        stack.push(token);
+    } else {
+        if (stack.length === 0) {
+            return { error: true, index: i };
+        }
 
-              let poppedToken = stack.pop();
-              if (checkSymbols[poppedToken] !== token) {
-                  return { error: true, index: i };
-              }
-          }
+        let poppedToken = stack.pop();
+        if (checkSymbols[poppedToken] !== token) {
+            return { error: true, index: i };
+        }
+    }
+}
+}
+
+if (stack.length > 0) {
+return { error: true, index: expr.length };
+}
+
+return { error: false, index: -1 };
+}
+
+//Convierte el arreglo original a postfijo
+function infixToPostfix(infix){
+const presedences = ["-", "+", "*", "/", "^", ">", "<",  ">=", "<=", "&&", "||"];
+
+var opsStack = [],
+  postfix = [];
+
+for(let token of infix){
+  // Step 1
+  
+  // if("number" === typeof token){
+  //if(presedences.includes(token)){
+  if(token != "+" && token != "*" && token != "-" && token != "/" && token != "(" && 
+  token != ")" && token != "^" && token != ">" && token != "<" && token != ">=" && 
+  token != "<=" && token != "&&" && token != "||"){
+      postfix.push(token); continue;
+  }
+  let topOfStack = opsStack[opsStack.length - 1];
+
+  // Step 2
+  // Si abre parentesis agregar a arreglo provisional
+  if(!opsStack.length || topOfStack == "("){
+      opsStack.push(token); continue;
+  }
+  // Step 3
+  if(token == "("){
+      opsStack.push(token); continue;
+  }
+  // Step 4
+  if(token == ")"){
+    //si cierra parentesis obtiene dato de arreglo provisional y lo mete a arreglo postfix
+      while(opsStack.length){
+          let op = opsStack.pop();
+          if(op == "(")	break;
+          postfix.push(op);
+      }
+      continue;
+  }
+  // Step 5
+  let prevPresedence = presedences.indexOf(topOfStack),
+      currPresedence = presedences.indexOf(token);
+  while(currPresedence < prevPresedence){
+      let op = opsStack.pop();
+      postfix.push(op);
+      prevPresedence = presedences.indexOf(opsStack[opsStack.length - 1]);
+  }
+  opsStack.push(token);
+}
+// Step 6
+while(opsStack.length){
+  let op = opsStack.pop();
+  if(op == "(")	break;
+  postfix.push(op);
+}
+
+return postfix;
+}
+
+// Método que divide la cadena principal en un arreglo con todos los componentes a evaluar
+function tokenize(exp){
+  exp = exp.replace(/\s/g, "");
+  //reemplazar || por OR para evaluar expresion regular
+  exp = exp.replace("||", "OR");
+
+  //Expresion regular que evalua y divide la cadena en un arreglo con todas las partes a covnertir a postfijo
+  var outs = exp.match(/\d+|OR|&&|<=|>=|[^]|[\+-\/x()<>=*]/g);
+
+  // For que sustituye "OR" por "||", se pone asi ya que en la expresion regular no se pudo reconocer "||".
+  for(var c = 0; c < outs.length; c++) {
+      if(outs[c] == "OR") {
+          outs[c] = "||";
       }
   }
-
-  if (stack.length > 0) {
-      return { error: true, index: expr.length };
-  }
-
-  return { error: false, index: -1 };
-}
-
-function evaluate(values) {
-    // var input = prompt("Please enter your input string\n\nExamples of input strings:\n\n\t1. 10 4 5 + *\n\t2. 10 4 5 + * 2 +\n\t3. 10 8 *");
-    // var values = input.split(" ");
-    var array = new Array();
-    for (i in values) {
-        if (values[i] != "+" && values[i] != "*" && values[i] != "-" && values[i] != "/") {
-            array.push(parseInt(values[i]));
-        } else {
-            var operator = values[i];
-            var val2 = array.pop();
-            var val1 = array.pop();
-            switch (operator) {
-                case "+":
-                    array.push(eval("val1 + val2"));
-                    break;
-                case "*":
-                    array.push(eval("val1 * val2"));
-                    break;
-                case "-":
-                    array.push(eval("val1 - val2"));
-                    break;
-                case "/":
-                    array.push(eval("val1 / val2"));
-                    break;
-            }
-        }
-    }
-    if (values.lenght == 0) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function infixToPostfix(infix){
-    const presedences = ["-", "+", "*", "/"];
-    
-	var opsStack = [],
-    	postfix = [];
-    
-    for(let token of infix){
-        // Step 1
-        
-    	// if("number" === typeof token){
-        if(token != "+" && token != "*" && token != "-" && token != "/" && token != "(" && token != ")" ){
-        	postfix.push(token); continue;
-        }
-        let topOfStack = opsStack[opsStack.length - 1];
-        // Step 2
-        if(!opsStack.length || topOfStack == "("){
-        	opsStack.push(token); continue;
-        }
-        // Step 3
-        if(token == "("){
-	        opsStack.push(token); continue;
-        }
-        // Step 4
-        if(token == ")"){
-        	while(opsStack.length){
-            	let op = opsStack.pop();
-                if(op == "(")	break;
-                postfix.push(op);
-            }
-            continue;
-        }
-        // Step 5
-		let prevPresedence = presedences.indexOf(topOfStack),
-        	currPresedence = presedences.indexOf(token);
-        while(currPresedence < prevPresedence){
-            let op = opsStack.pop();
-            postfix.push(op);
-            prevPresedence = presedences.indexOf(opsStack[opsStack.length - 1]);
-        }
-        opsStack.push(token);
-	}
-    // Step 6
-    while(opsStack.length){
-        let op = opsStack.pop();
-        if(op == "(")	break;
-        postfix.push(op);
-    }
-    
-    return postfix;
-}
-
-function tokenize(exp){
-    exp = exp.replace(/\s/g, "");
-    var outs = exp.match(/\d+\s?\d+|\d+|[\+-\/x()*]/g);
-    return outs;
+  return outs;
 }
 
 function log(obj){
-	document.querySelector("pre").textContent += JSON.stringify(obj) + "\n";
+document.querySelector("pre").textContent += JSON.stringify(obj) + "\n";
 }
 
+//Este metodo resuelve el problema teniendolo ya en postfijo
 function RPN(seq) {
 
-    if (seq.length <= 2) {
-      console.log('Please enter valid RPN');
-      return
-    }
-  
-    var operands = ['+', '-', '*', '/' ]
-    var stack = []
-    var i = 0
-  
-    stack.push(seq[i])
-    i++
-  
-    while(i <= seq.length) {
-      var item = seq[i]
-      var index = operands.indexOf(item)
-      if (index < 0) {
-        stack.push(seq[i])
-      } else {
-        if (index == 0) {
-          var a = parseInt(stack.splice(-1)[0], 10)
-          var b = parseInt(stack.splice(-1)[0], 10)
-          stack.push(a+b)
-          
-        }
-        if (index == 1) {
-        var a = parseInt(stack.splice(-1)[0], 10)
-          var b = parseInt(stack.splice(-1)[0], 10)
-          stack.push(b-a)
-          
-        }
-        if (index == 2) {
-        var a = parseInt(stack.splice(-1)[0], 10)
-          var b = parseInt(stack.splice(-1)[0], 10)
-          stack.push(a*b)
-          
-        }
-        if (index == 3) {
-        var a = parseInt(stack.splice(-1)[0], 10)
-          var b = parseInt(stack.splice(-1)[0], 10)
-          stack.push(b/a)
-          
-        }
-      }
-       i++
-    }
-  
-    return parseInt(stack[0],10)
-}//;
+if (seq.length <= 2) {
+console.log('Please enter valid RPN');
+return
+}
 
-function validarErrSig(ecu){
-    let erss = [];
-    let ers = 0;
-    let signos = ['{','[', '(', ')', ']', '}','+', '-', '*', '/', ' ','1','2','3','4','5','6','7','8','9','0'];
+//operandos posibles
+var operands = ['+', '-', '*', '/', '^', '<', '>', '<=', '>=', '&&', '||' ]
+var stack = []
+var i = 0
 
-    for (let i = 0; i < ecu.length; i++) {
-      if(signos.includes(ecu[i])){
-        
-      }else{
-        erss.push(ecu[i]);
-        ers++;
-      }
-    }
+stack.push(seq[i])
+i++
 
-    if(ers > 0){
-      return {consigers: true ,  erers: erss} 
-    }else{
-      return {consigers: false ,  erers: 'Todo Correcto'} 
-    }
+//ciclo para recorrer todos los elementos que estan en postfijo
+while(i <= seq.length) {
+var item = seq[i]
+//console.log(item)
+var index = operands.indexOf(item)
+//Si no hay nada se agrega a stack
+if (index < 0) {
+  stack.push(seq[i])
+} else {
+  //si el caracter es un operando, hacer la operacion correspondiente
+  var a = parseInt(stack.splice(-1)[0], 10)
+  var b = parseInt(stack.splice(-1)[0], 10)
+  if (index == 0) {
+    stack.push(a+b)
+  }
+  if (index == 1) {
+    stack.push(b-a)
+  }
+  if (index == 2) {
+    stack.push(a*b)
+  }
+  if (index == 3) {
+    stack.push(b/a)
+  }
+  if(index == 4) {
+      stack.push(Math.pow(parseInt(b), parseInt(a)))
+  } 
+  //verifica los operandos logicos
+  if(index == 5) {
+      stack.push(b < a);
+  }
+  if(index == 6) {
+      stack.push(b > a);
+  }
+  if(index == 7) {
+      stack.push(b <= a);
+  }
+  if(index == 8) {
+      stack.push(b >= a);
+  }
+  if(index == 9) {
+      stack.push(b && a);
+  }
+  if(index == 10) {
+      stack.push(b || a);
+  }
+}
+ i++
+}
+
+return parseInt(stack[0],10)
+};
+
+//Valida si los caracteres son correctos, caracteres ajenos van a ser invalidos
+function validarErrSig(ecu){ // 2 + 2
+let erss = [];
+let ers = 0;
+let signos = ['{','[', '(', ')', ']', '}','+', '-', '*', '/', ' ','=', '^','|','||','&','&&', '<', '>', '<>', '<=', '>=','1','2','3','4','5','6','7','8','9','0'];
+
+for (let i = 0; i < ecu.length; i++) {
+if(signos.includes(ecu[i])){
+  
+}else{
+  erss.push(ecu[i]);
+  ers++;
+}
+}
+
+if(ers > 0){
+return {consigers: true ,  erers: erss} 
+}else{
+return {consigers: false ,  erers: 'Todo Correcto'} 
+}
 
 }
 
+Array.prototype.unique=function(a){
+return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+});
 
 main();
